901c615502fcacc280c623bb83fc0e04941bb508
James Ward <james@jamesward.com>
2023-09-15 15:51:13 -0600

    init

---
+++ build.sbt
@@ -0,0 +1,9 @@
+scalaVersion := "3.3.1"
+
+val zioVersion = "2.0.16"
+
+libraryDependencies ++= Seq(
+  "dev.zio" %% "zio"          % zioVersion,
+  "dev.zio" %% "zio-test"     % zioVersion % Test,
+  "dev.zio" %% "zio-test-sbt" % zioVersion % Test,
+)
---
+++ src/test/scala/MySpec.scala
@@ -0,0 +1,36 @@
+import zio.test.*
+import zio.test.Assertion.*
+
+object MySpec extends ZIOSpecDefault:
+  def spec = suite("My")(
+    test("stuff") {
+      case class Role(title: String)
+      case class Person(name: String, role: Role)
+      val people = Seq(
+        Person("Joe", Role("Engineer")),
+        Person("Sally", Role("Manager")),
+        Person("Bob", Role("QA"))
+      )
+      val rightSomeTwo: Either[_, Option[Int]] = Right(Some(2))
+
+      assert(people.size)(equalTo(3)) &&
+      assert(people)(hasSize(equalTo(3))) &&
+      // vs
+      assertTrue(people.size == 3) &&
+      //
+      //
+      assert(people)(exists(hasField("role", _.role.title, equalTo("Engineer")))) &&
+      // vs
+      assertTrue(people.exists(_.role.title == "Engineer")) &&
+      //
+      //
+      assert(people.head.role.title)(equalTo("Engineer")) &&
+      // vs
+      assertTrue(people.head.role.title == "Engineer") &&
+      //
+      //
+      assert(rightSomeTwo)(isRight(isSome(equalTo(2)))) &&
+      // vs
+      assertTrue(rightSomeTwo.exists(_.contains(2)))
+    }
+  )
