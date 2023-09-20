a3696efbdc200582f0fa6882568f7e873ae8d25a
James Ward <james@jamesward.com>
2023-09-20 17:49:51 -0600

    add gradleable

---
+++ gradleable/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("com.bnorm.power.kotlin-power-assert") version "0.13.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<Test> {
+    testLogging {
+        showStandardStreams = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        events(org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED, org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED, org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED, org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED)
+    }
+}
---
+++ gradleable/src/test/kotlin/MyTest.kt
@@ -0,0 +1,18 @@
+import kotlin.test.Test
+
+class MyTest {
+
+    @Test
+    fun one() {
+        data class Role(val title: String)
+        data class Person(val name: String, val role: Role)
+        val people = listOf(
+                Person("Joe", Role("Engineer")),
+                Person("Sally", Role("Manager")),
+                Person("Bob", Role("QA")),
+        )
+
+        assert(people.find { it.name == "Ralph" }!!.role.title.equals("qa", true))
+    }
+
+}
